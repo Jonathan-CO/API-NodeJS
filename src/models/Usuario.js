000fd4c3a67311a3bb390e9e9d2a60ee641c9c94
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2019-10-11 11:59:39 -0300

    define model Usuario

---
+++ src/models/Usuario.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    sex: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    birthdate:{
+        type: Date,
+        required: true
+    },
+    tel:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    address:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    active:{
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tags:[{
+        type: String,
+        required: true,
+        trim: true
+    }]
+})
+
+module.exports = mongoose.model('Usuarios', schema)
