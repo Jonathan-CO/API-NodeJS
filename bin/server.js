2d412bd66eafcab9d56f4457e22c500a7f5beebc
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2019-10-10 21:36:12 -0300

    separa server do app

--- bin/server.js
+++ bin/server.js
@@ -1,29 +1,15 @@
-const http = require ('http')
-const express = require ('express')
+const app = require('../src/app')
 const debug = require ('debug')('nodestr:server')
+const http = require ('http')
 
-const app = express()
 const port = normalizePort(process.env.PORT || 3000)
 app.set('port', port)
 
-
-
 const server = http.createServer(app)
-const router = express.Router()
-
-const route = router.get('/', (req, res, next)=>{
-    res.send({
-        title: "Node Store API",
-        version: '0.0.1'
-    });
-});
-
-app.use('/', route)
 
 server.listen(port)
 server.on('listening', onListening)
 
-console.log('ok')
 function normalizePort(val){ // baseada no gerador de código do express
     const port = parseInt(val, 10); // valor na base 10
     if(isNaN(port)){
@@ -40,3 +26,4 @@ function onListening(){
     const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
     debug('Listening on '+bind);
 }
+
