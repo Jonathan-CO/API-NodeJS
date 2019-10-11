619b26e247b1f14e9db7e94621f5b54587480a0c
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2019-10-10 23:14:07 -0300

    Cria controller e passa ações para os mesmos

--- src/routes/pessoa-route.js
+++ src/routes/pessoa-route.js
@@ -1,21 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.post('/', (req, res, next)=>{
-    res.status(201).send(req.body) // status 201 = created
-    console.log(req.body)
-});
+const controller = require('../controllers/pessoa-controller')
 
-router.put('/:id', (req, res, next)=>{
-    const id = req.params.id
-    res.status(200).send({ // status 201 = created
-        id: id,
-        item: req.body 
-    })
-});
+router.post('/', controller.post);
 
-router.delete('/', (req, res, next)=>{
-    res.status(200).send(req.body) // status 201 = created
-});
+router.put('/:id', controller.put);
+
+router.delete('/', controller.delete);
 
 module.exports = router
