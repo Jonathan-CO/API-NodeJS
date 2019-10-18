ab7950ba1cf416ac757d09d4c5f031ddde8523d3
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2019-10-17 21:04:25 -0300

    habilita CORS

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const mongoose = require('../bin/db')
 const User = require('./models/User')
 const Group = require('./models/Group')
 const Event = require('./models/Event')
-//const Work = require('./models/Work')
 
 //Carrega rotas
 const indexRoute = require('./routes/index-route')
@@ -19,9 +18,21 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 
+//Habilita CORS
+app.use(function (req, res, next){
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, x-access-token')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    next()
+})
+
+
+
 app.use('/', indexRoute)
 app.use('/users', userRoute)
 app.use('/groups', groupRoute)
 app.use('/events', eventRoute)
+
+
 
 module.exports = app
